3e168893599551ec8d684aca8491155dac7d31f3
DasMonitor <davidneumann2155@gmail.com>
2024-08-10 22:41:25 +0200

    minor code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::cmp::max;
 use std::num::NonZeroUsize;
-use std::os;
 use std::thread;
-use std::thread::Thread;
 use random::{Source, Xorshift128Plus};
 use std::time::{Instant, SystemTime};
 
@@ -20,11 +18,11 @@ fn run_cycles(amount_of_cycles: usize) -> u64 {
         }
     );
 
-    let mut count_of_0: u64 = 0;
+
     let mut max_count_of_0: u64 = 0;
 
     for _ in 1..=amount_of_cycles {
-        count_of_0 = 0;
+        let mut count_of_0: u64 = 0;
         for _ in 1..=231 {
             let next = sequence.read_u64() % 4;
             if next == 0 { count_of_0 += 1}
@@ -35,7 +33,7 @@ fn run_cycles(amount_of_cycles: usize) -> u64 {
     max_count_of_0
 }
 
- fn solution_v2(){
+ pub fn solution_v2(){
     let start = Instant::now();
 
     let amount_of_threads = thread::available_parallelism()
@@ -43,12 +41,12 @@ fn run_cycles(amount_of_cycles: usize) -> u64 {
             unsafe {NonZeroUsize::new_unchecked(8)}.into()
         ).get();
 
-    println!("Recomended amount of threads: {amount_of_threads}");
+    println!("Recommended amount of threads: {amount_of_threads}");
 
     let mut threads = Vec::with_capacity(amount_of_threads);
 
     let regular_amount_of_cycles = AMOUNT_OF_CYCLES/amount_of_threads;
-    let residular_amount_of_cycles = regular_amount_of_cycles + AMOUNT_OF_CYCLES
+    let residual_amount_of_cycles = regular_amount_of_cycles + AMOUNT_OF_CYCLES
         - regular_amount_of_cycles * amount_of_threads;
 
     let thread_closure = move || {run_cycles(regular_amount_of_cycles)};
@@ -57,7 +55,7 @@ fn run_cycles(amount_of_cycles: usize) -> u64 {
         threads.push(thread::spawn(thread_closure));
     }
 
-    let own_max = run_cycles(residular_amount_of_cycles);
+    let own_max = run_cycles(residual_amount_of_cycles);
 
     let mut maxes: Vec<u64> = threads.into_iter().map(
         |t| t.join().unwrap()
@@ -69,7 +67,7 @@ fn run_cycles(amount_of_cycles: usize) -> u64 {
 
      // Note: Due to the abstract nature of the randomization, a value of zero is associated with
      // getting a proper value of 1
-     println!("Max ammount of 1: {}", overall_max);
+     println!("Max amount of 1: {}", overall_max);
 
     let stop = start.elapsed();
     println!("Time taken: {:?}", stop)
@@ -77,7 +75,7 @@ fn run_cycles(amount_of_cycles: usize) -> u64 {
 
 // primitive solution first written
 // Duration ~ 600 s
-fn solution_v1(){
+pub fn solution_v1(){
     let start = Instant::now();
     let mut sequence: Xorshift128Plus = random::default(
         unsafe {
@@ -85,11 +83,10 @@ fn solution_v1(){
         }
     );
 
-    let mut count_of_0: u64 = 0;
     let mut max_count_of_0: u64 = 0;
 
     for _ in 1..=AMOUNT_OF_CYCLES{
-        count_of_0 = 0;
+        let mut count_of_0: u64 = 0;
         for _ in 1..=231 {
             let next = sequence.read_u64() % 4;
             if next == 0 { count_of_0 += 1}
@@ -100,7 +97,7 @@ fn solution_v1(){
 
     // Note: Due to the abstract nature of the randomization, a value of zero is associated with
     // getting a proper value of 1
-    println!("Max ammount of 1: {}", max_count_of_0);
+    println!("Max amount of 1: {}", max_count_of_0);
     let stop = start.elapsed();
     println!("Time taken: {:?}", stop);
 }
